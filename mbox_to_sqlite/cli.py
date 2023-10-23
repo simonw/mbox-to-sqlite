fd7fdedce20573dc66738bbacced10f7de4c7db5
Peter Kropf <pkropf@pm.me>
2023-10-23 16:38:41 -0700

    added the --lowercol option to force the table column names to be lower case. Allows for importing Google Takeout .mbox files as they will mix the case of email header fields.

--- mbox_to_sqlite/cli.py
+++ mbox_to_sqlite/cli.py
@@ -24,22 +24,32 @@ def cli():
 )
 @click.option("--table", default="messages")
 @click.option("--tracer", is_flag=True)
+@click.option("--lowercol", is_flag=True)
 
-def mbox(db_path, mbox_path, table, tracer):
+def mbox(db_path, mbox_path, table, tracer, lowercol):
     "Import messages from an mbox file"
+    print('hello')
     if tracer:
         db = sqlite_utils.Database(db_path, tracer=sql_tracer)
     else:
         db = sqlite_utils.Database(db_path)
     mbox = mailbox.mbox(mbox_path)
 
+    if lowercol:
+        message_id = "message-id"
+    else:
+        message_id = "Message-ID"
+
     def to_insert():
         for message in mbox.values():
-            row = dict(message.items())
+            if lowercol:
+                row = dict([(l.lower(), r) for l,r in message.items()])
+            else:
+                row = dict(message.items())
             row["payload"] = message.get_payload()
             yield row
 
-    db[table].upsert_all(to_insert(), alter=True, pk="Message-ID")
+    db[table].upsert_all(to_insert(), alter=True, pk=message_id)
 
     if not db[table].detect_fts():
         db[table].enable_fts(["payload", "subject"], create_triggers=True)
